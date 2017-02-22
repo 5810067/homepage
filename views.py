c9da8496875a7e0532bcd104110a877524e8ab02
a5810067 <a5810067@gmail.com>
2017-02-22 20:57:05 +0700

    add new files can show pic from static folder

--- views.py
+++ views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect,HttpResponse
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+def show_home(request):
+    return render(request, 'homepage/homepage.html')
+    
